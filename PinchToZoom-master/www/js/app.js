2c0ba0238736c8d77fc783475cc3a9f4a6a96d31
zafercan <zafercandeli@gmail.com>
2015-06-30 14:10:55 +0300

    share

--- PinchToZoom-master/www/js/app.js
+++ PinchToZoom-master/www/js/app.js
@@ -1,86 +1,134 @@
-var nameApp = angular.module('starter', ['ionic', 'ui.router']);
- 
+var nameApp = angular.module('starter', ['ionic', 'ui.router', 'ngCordova']);
+nameApp.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
 nameApp.config(function($stateProvider, $urlRouterProvider) {
-  
-  $stateProvider
-    .state('index', {
-      url: '/',
-      templateUrl: 'index.html',
-      controller: 'ListCtrl'
-    })
-    .state('view', {
-      url: '/images',
-      templateUrl: 'yedekImage.html',
-      controller: 'MainCtrl'
-    }) ;
-  
-  $urlRouterProvider.otherwise("/");
-  
+
+    $stateProvider.state('index', {
+        url : '/',
+        templateUrl : 'index.html',
+        controller : 'ListCtrl'
+    }).state('view', {
+        url : '/images',
+        templateUrl : 'yedekImage.html',
+        controller : 'MainCtrl'
+    }).state('share', {
+        url : '/share',
+        templateUrl : 'share.html',
+        controller : 'ShareController'
+    });
+
+    $urlRouterProvider.otherwise("/");
+
 });
- 
-nameApp.controller('ListCtrl', function($scope, $state) {
-  $scope.changePage = function(){
-    $state.go('view', {movieid: 1});
-  };    
+
+nameApp.controller('ListCtrl', function($scope, $state, $http) {
+    $scope.changePage = function() {
+        $state.go('view', {
+            imageid : 1
+        });
+    };
+
 });
-  
+
 nameApp.controller('ViewCtrl', function($scope, $stateParams, $ionicHistory) {
-  console.log($stateParams.movieid);
-  $scope.goBack = function(){
-    $ionicHistory.goBack();
-  };    
+    console.log($stateParams.imageid);
+    $scope.goBack = function() {
+        $ionicHistory.goBack();
+    };
 });
 
-nameApp.controller('MainCtrl', function($scope, $ionicModal) {
-	$scope.hide = [{
-		bars : true
-	}];
+nameApp.controller('MainCtrl', function($scope, $ionicModal, $http) {
 
+    $scope.hide = [{
+        bars : true
+    }];
 
-	$scope.data = {};
-	$scope.grids = grids;
+    $scope.data = {};
+    $scope.grids = grids;
 
-	// $scope.grids = [{
-	// id: 0,
-	// src:  deneme,
-	// }, {
-	// id: 1,
-	// src:  'http://lorempixel.com/400/200/sports/2/',
-	// }, {
-	// id: 2,
-	// src: 'http://lorempixel.com/400/200/sports/3/',
-	// }, {
-	// id: 3,
-	// src: 'http://lorempixel.com/400/200/sports/4/',
-	// }, {
-	// id: 4,
-	// src: 'http://lorempixel.com/400/200/sports/5/',
-	// }, {
-	// id: 5,
-	// src: 'http://lorempixel.com/400/200/sports/6/',
-	// }];
+    // $scope.grids = [{
+    // id: 0,
+    // src:  deneme,
+    // }, {
+    // id: 1,
+    // src:  'http://lorempixel.com/400/200/sports/2/',
+    // }, {
+    // id: 2,
+    // src: 'http://lorempixel.com/400/200/sports/3/',
+    // }, {
+    // id: 3,
+    // src: 'http://lorempixel.com/400/200/sports/4/',
+    // }, {
+    // id: 4,
+    // src: 'http://lorempixel.com/400/200/sports/5/',
+    // }, {
+    // id: 5,
+    // src: 'http://lorempixel.com/400/200/sports/6/',
+    // }];
 
-	$ionicModal.fromTemplateUrl('templates/modal.html', function(modal) {
-		$scope.gridModal = modal;
-	}, {
-		scope : $scope,
-		animation : 'slide-in-up'
-	});
+    $ionicModal.fromTemplateUrl('templates/modal.html', function(modal) {
+        $scope.gridModal = modal;
+    }, {
+        scope : $scope,
+        animation : 'slide-in-up'
+    });
 
-	// $scope.initialize = function(grids) {
-		// console.log();
-		// $scope.grids = grids;
-	// };
+    // $scope.initialize = function(grids) {
+    // console.log();
+    // $scope.grids = grids;
+    // };
 
-	$scope.openModal = function(selected) {
-		console.log(selected.id);
-		$scope.data.selected = selected.id;
+    $scope.openModal = function(selected) {
+        console.log(selected.id);
+        $scope.data.selected = selected.id;
 
-		$scope.gridModal.show();
-	};
+        $scope.gridModal.show();
+    };
 
-	$scope.closeModal = function() {
-		$scope.gridModal.hide();
-		$scope.hide.bars = false;
-	};
+    $scope.closeModal = function() {
+        $scope.gridModal.hide();
+        $scope.hide.bars = false;
+    };
+
+    $scope.doRefresh = function() {
+
+        $http.get("https://dl.dropboxusercontent.com/u/1403240/staj/metadata.json")
+
+        //angular.element(document.getElementById('main')).scope().initialize(grids);
+
+        .success(function(data) {
+
+            var URLs = [];
+            var grids;
+
+            var deviceName = data.deviceName;
+            var raspiCamCount = data.raspiCamCount;
+
+            //$("#imageTitle").innerHTML = "";
+
+            for (var i = 0; i < raspiCamCount; i++) {
+
+                URLs[i] = data.cams[i].url;
+                grids[i].srcImage = URLs[i];
+                grids[0].title = deviceName;
+
+                //document.getElementById("imageTitle").innerHTML = deviceName;
+                //$("#images").append("<li class=\"item\" ><img src=' " + URLs[i] + " ' width='100%' heigth='100%'></li> ");
+            }
+        }).finally(function() {
+
+            // Stop the ion-refresher from spinning
+            $scope.$broadcast('scroll.refreshComplete');
+        });
+    };
+
 });
+
